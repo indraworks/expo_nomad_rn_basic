bd5f3272909394d027e7f693adfaa64b8cb2f7d7
indra <indra.works2020@gmail.com>
2024-10-23 16:51:26 +0700

    screenOptions(pada tab.navigator) & screen pada tab,screen utk tabbarlabelStyle

--- src/navigation/Tabs.js
+++ src/navigation/Tabs.js
@@ -5,10 +5,45 @@ import SearchScreen from "../screens/SearchScreen";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movie" component={MovieScreen} />
+  <Tab.Navigator
+    initialRouteName="Tv!"
+    screenOptions={{
+      tabBarLabelStyle: {
+        backgroundColor: "red",
+      },
+    }}
+  >
+    <Tab.Screen
+      name="Movie"
+      component={MovieScreen}
+      options={{
+        tabBarLabelStyle: {
+          backgroundColor: "purple",
+        },
+      }}
+    />
     <Tab.Screen name="Tv!" component={TvScreen} />
     <Tab.Screen name="Search" component={SearchScreen} />
   </Tab.Navigator>
 );
 export default Tabs;
+
+/*
+utk navgation kita ada kenal dengan initialROuteName itu artinya 
+yg akan dituju atau dirender misalkan diatas awal pasti defaultnya adalah movie 
+tapu dngn kita set ditab.navigator bisa ke Tv ,jadi pd start awal dia akan ke screen Tv 
+
+-options adalah sama jika kita mau utak atik di tab,navigator utk smua tab misalkan tabBarLabelStyle 
+aetinya tulisan/label  yg ada pada tabBar stylenya kita atur 
+utk atur berlaku pada smua sceeen set di tabNavigatior
+screenOptions={{
+    tabBarLabelStyle: { fontSize: 12 },
+    tabBarItemStyle: { width: 100 },
+    tabBarStyle: { backgroundColor: 'powderblue' },
+  }}
+>
+utk berlaku hanya pada scteen ya saja maka dgn =option set di tab,screen-nya 
+
+
+
+*/
